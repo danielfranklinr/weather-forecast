bd73b56fa64770f690d439515175c8f74cc2dfb6
Daniel Franklin <danielfranklinr@gmail.com>
2024-04-13 16:30:11 -0500

    fix: removing unnecesary code

--- app/src/main/java/com/dfr/weather/data/provider/DeviceLocationProviderImpl.kt
+++ app/src/main/java/com/dfr/weather/data/provider/DeviceLocationProviderImpl.kt
@@ -41,23 +41,18 @@ class DeviceLocationProviderImpl @Inject constructor(
                 }
 
                 override fun onLocationChanged(locations: MutableList<Location>) {
-                    super.onLocationChanged(locations)
                 }
 
                 override fun onFlushComplete(requestCode: Int) {
-                    super.onFlushComplete(requestCode)
                 }
 
                 override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
-                    super.onStatusChanged(provider, status, extras)
                 }
 
                 override fun onProviderEnabled(provider: String) {
-                    super.onProviderEnabled(provider)
                 }
 
                 override fun onProviderDisabled(provider: String) {
-                    super.onProviderDisabled(provider)
                 }
             }
 
